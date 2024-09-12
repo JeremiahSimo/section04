ae9850ab8382812c983518fe2cbb893825677dc7
unknown <jeremiahsimo26@gmail.com>
2024-09-12 08:13:47 +0800

    updated label sa form

--- new.php
+++ new.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    
+
     <title>Document</title>
 </head>
 <body>
-/* From Uiverse.io by zanina-yassine */ 
+/* Payment Form */ 
 <div class="modal">
 <form class="form">
   <div class="payment--options">
